a05b3fc0cec2459335cc7a8644f5021869e67d5b
Daniel Valle <israelvalle48@gmail.com>
2017-06-20 23:24:46 +0800

    issuance UI

--- resources/views/transaction/transaction-issuance.blade.php
+++ resources/views/transaction/transaction-issuance.blade.php
@@ -22,28 +22,76 @@
 
                         <div class="panel-body">
                             {!! Form::open(['class' => 'form-inline', 'method' => 'post', 'url' => 'transaction/abstract-quotation-search']) !!}
-                        <div class="form-group" style="width: 100%">   
-                            <div class="form-group" style="width: 50%; float: left">
-                                <div class="form-group" style="width: 100%">
-                                    <label for="" style="width: 30%">Issuance Number:</label>
-                                    <input class="form-control" type="text" readonly>                               
-                                </div>
-                            </div>
+                        <div class="form-group">   
 
-                            <div class="form-group" style="width: 50%; float: left">                          
-                                <div class="form-group" style="width: 100%">
-                                    <label for="" style="width: 10%">Agency:</label>
+                            <div class="form-group" style="margin-right: 6px">                          
+                                <div class="form-group">
+                                    <label for="">Agency</label>
                                     <select class="selectpicker" name="select-rfq-no" id="select-rfq-no">
                                     </select>                              
                                 </div>
                             </div>
+
+                            <div class="form-group" style="margin-right: 6px">                          
+                                <div class="form-group">
+                                    <label for="">Department</label>
+                                    <select class="selectpicker" name="select-rfq-no" id="select-rfq-no">
+                                    </select>                              
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 6px">                          
+                                <div class="form-group">
+                                    <label for="">Office</label>
+                                    <select class="selectpicker" name="select-rfq-no" id="select-rfq-no">
+                                    </select>                              
+                                </div>
+                            </div>
+
+                             <div class="form-group">                          
+                                <div class="form-group">
+                                    <label for="">Reasonability Center</label>
+                                    <input type="text" class="form-control">                              
+                                </div>
+                            </div>
                         </div>
-                            {!! Form::close() !!}
-                        </div>             
+
+                        <div style="margin: 20px 0"></div>
+
+                        <div class="form-group">   
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">RIS No.</label>
+                                    <input type="text" class="form-control">                            
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">RIS Date</label>
+                                    <input type="date" class="form-control">                           
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">SAI No.</label>
+                                    <input type="text" class="form-control">                            
+                                </div>
+                            </div>
+
+                             <div class="form-group">                          
+                                <div class="form-group">
+                                    <label for="">SAI Date.</label>
+                                    <input type="date" class="form-control">                              
+                                </div>
+                            </div>
+                        </div>
 
                          <div class="panel-body">
                             <div class="panel panel-default">
-                                <div class="panel-heading"></div>
+                                <div class="panel-heading"><button type="button" class="btn btn-success btn-sm" data-toggle="modal" data-target="#add-item">Add New Item</button> </div>
                                 <div class="panel-body">
                                     <div class="form-group col-lg-12">
                                         <table class="table table-bordered">
@@ -67,6 +115,165 @@
                                 </div>             
                             </div>
                         </div>
+
+                        <div class="form-group" style="width: 100%">   
+
+                            <div class="form-group" style="margin-right: 50px; width: 100%">                          
+                                <div class="form-group" style="width: 100%">
+                                    <label for="">Purpose</label>
+                                    <textarea style="width: 90%" class="form-control"></textarea>                      
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <div style="margin: 20px 0"></div>
+
+                        <div class="form-group">
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Requested By</label>
+                                    <select class="selectpicker"></select>                                                  
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Requestor Designation</label>
+                                    <select class="selectpicker"></select>                         
+                                </div>
+                            </div>
+
+                             <div class="form-group">                          
+                                <div class="form-group">
+                                    <label for="">Request Date</label>
+                                    <input type="date" class="form-control">                              
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="margin: 20px 0"></div>
+
+                        <div class="form-group">
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Requested By</label>
+                                    <select class="selectpicker"></select>                                                  
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Requestor Designation</label>
+                                    <select class="selectpicker"></select>                         
+                                </div>
+                            </div>
+
+                             <div class="form-group">                          
+                                <div class="form-group">
+                                    <label for="">Request Date</label>
+                                    <input type="date" class="form-control">                              
+                                </div>
+                            </div>
+                        </div>
+
+                        <div style="margin: 20px 0"></div>
+
+                        <div class="form-group">
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Approved By</label>
+                                    <select class="selectpicker"></select>                                                  
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Approver Designation</label>
+                                    <select class="selectpicker"></select>                         
+                                </div>
+                            </div>
+
+                             <div class="form-group">                          
+                                <div class="form-group">
+                                    <label for="">Approved Date</label>
+                                    <input type="date" class="form-control">                              
+                                </div>
+                            </div>
+                        </div>                        
+                        
+                        <div style="margin: 20px 0"></div>
+
+                        <div class="form-group">
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Issued By</label>
+                                    <select class="selectpicker"></select>                                                  
+                                </div>
+                            </div>
+
+                            <div class="form-group" style="margin-right: 50px">                          
+                                <div class="form-group">
+                                    <label for="">Issuer Designation</label>
+                                    <select class="selectpicker"></select>                         
+                                </div>
+                            </div>
+
+                             <div class="form-group">                          
+                                <div class="form-group">
+                                    <label for="">Issued Date</label>
+                                    <input type="date" class="form-control">                              
+                                </div>
+                            </div>
+                        </div>
+
+                        </div>             
+                        {!! Form::close() !!}
+
+                        <div id="add-item" class="modal fade" role="dialog">
+                            <div class="modal-dialog">
+                                {!! Form::open(['url' => 'transaction/purchase-request/add-item', 'method' => 'post']) !!}
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h4 class="modal-title">Add New Item</h4>
+                                    </div>
+                                    <div class="modal-body container-fluid">
+                                        <div class="col-lg-12">
+                                            <div class="form-group col-lg-6">
+                                                <label for="">Item:</label>
+                                                <select class="form-control" name="add-item" id="add-item">
+
+                                                </select>
+                                            </div>        
+                                            <div class="form-group col-lg-3">
+                                                <label for="">Quantity</label>
+                                                <input type="number" class="form-control" name="add-quantity" id="add-quantity" required>
+                                            </div>   
+                                            <div class="form-group col-lg-3">
+                                                <label for="">Unit</label>
+                                                <select class="form-control" name="add-unit" id="add-unit">
+
+                                                </select>
+                                            </div>    
+                                        </div>
+                                        <div class="col-lg-12">
+                                            <div class="form-group col-lg-12">
+                                                <label for="">Remarks:</label>
+                                                <textarea type="text" class="form-control"></textarea>
+                                            </div>        
+                                        </div>
+
+
+                                    </div>
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn btn-success">Add</button>
+                                            <button type="button" class="btn btn-default" data-dismiss="modal" id="modal-close">Close</button>
+                                        </div>
+                                </div>
+                                {!! Form::close() !!}
+                            </div>
+                        </div>
+
                     </div>
                     <!-- /.panel -->
                 </div>
