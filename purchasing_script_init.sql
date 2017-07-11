202c62bc3fae7d8ae4a82c43d799b12b869cf515
Daniel Valle <daniel.valle@h2software.co>
2017-07-11 17:19:12 +0800

    updated script

--- purchasing_script_init.sql
+++ purchasing_script_init.sql
@@ -111,6 +111,14 @@ CREATE TABLE user
        FOREIGN KEY (designation_fk) REFERENCES designation(id)
     );
     
+INSERT INTO agency(agency_name, is_active, created_at, updated_at) values ("Main Agency", 1, NOW(), NOW());
+INSERT INTO designation(designation_name, is_active, created_at, updated_at) values ("Administrator", 1, NOW(), NOW());
+INSERT INTO user(first_name, last_name, sex, email, birthday, password, agency_fk, designation_fk, user_type, is_active, created_at, updated_at)
+VALUES ("Super", "Admin", "M", "superadmin@gmail.com", NOW(), "$2y$10$cwli4dox7NjqBLptzb6jjOkT9Wj5h69wDjkDwWfhtq4c9o2/ercXi", 
+        (SELECT id from agency WHERE agency_name = "Main Agency" LIMIT 1), 
+        (SELECT id from designation WHERE designation_name = "Administrator" LIMIT 1), 
+        1, 1, NOW(), NOW())
+
 CREATE TABLE supplier
   (
       `id` int NOT NULL AUTO_INCREMENT, 
